7f61b33cddcebf42c081155e8d9387abac5305fa
Yena-Yun <cheryleduart@gmail.com>
2021-08-16 16:01:18 +0900

    react-bbootstrap, 쇼핑몰항목(map)

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,64 @@
-import logo from "./logo.svg";
+/* eslint-disable */
+import React, { useState } from "react";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
 import "./App.css";
+import data from "./data";
 
 function App() {
-  return <div className="App">쇼핑몰 만들기</div>;
+  let [shoes, shoes변경] = useState(data);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((shoe, i) => (
+            <Card shoes={shoe} idx={i} key={i} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
+
+const Card = ({ shoes, idx }) => {
+  return (
+    <div className="col-md-4">
+      {/* 중간에 ${} 쓰고 싶으면 그 줄 전체를 { }로 감싸야 */}
+      <img src={`https://codingapple1.github.io/shop/shoes${idx + 1}.jpg`} alt="" width="100%" />
+      <h4>{shoes.title}</h4>
+      <p>
+        {shoes.content} & {shoes.price}
+      </p>
+    </div>
+  );
+};
 
 export default App;
---
+++ shop/src/ShopItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ShopItem = (props) => {
+  return <ShopItemBlock></ShopItemBlock>;
+};
+
+const ShopItemBlock = styled.div``;
+
+export default ShopItem;
